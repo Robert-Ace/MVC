8593c5eed8b6a51c43ac9d5d59efc953d749a517
Robert-Ace <beeeshop@yandex.ru>
2023-05-22 22:52:10 +0300

    Начало класса для работы с БД

--- application/core/AbstractController.php
+++ application/core/AbstractController.php
@@ -3,14 +3,18 @@
 namespace application\core;
 
 use application\core\View;
+use application\lib\Db;
 
 abstract class AbstractController
 {
     public array $routeParams;
     public View $view;
+    private Db $db;
+
     public function __construct($routeParams)
     {
         $this->routeParams = $routeParams;
         $this->view = new View($routeParams);
+        $this->db = new Db();
     }
 }
---
+++ application/lib/Db.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace application\lib;
+
+use PDO;
+
+class Db
+{
+    private PDO $db;
+    public function __construct()
+    {
+        $config = require 'application/config/db.php';
+        $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
+        $this->db = new PDO($dsn, $config['user'], $config['password'], $config['opt']);
+    }
+
+    private function query(string $sql, $params = []) : bool|\PDOStatement
+    {
+        $stmt = $this->db->prepare($sql);
+        if (!empty($params)) {
+            foreach ($params as $key => $val) {
+                $stmt->bindValue(":{$key}", $val);
+            }
+        }
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function getRow(string $sql, $params = []) : bool|array
+    {
+        return $this->query($sql, $params)->fetchAll();
+    }
+
+    public function getCollumn(string $sql, $params = []) : bool|array
+    {
+        return $this->query($sql, $params)->fetchColumn();
+    }
+
+}
